0aba951058e41de049880d2e55a5f4e8d5b1f4f0
CC9898A <cirpetrosino98@gmail.com>
2022-02-14 12:47:41 +0100

    esercizio

---
+++ s4/g1/JS/script.js
@@ -0,0 +1,36 @@
+//variable list
+var variabile_prova="io sono una variabile"
+
+var variabile_prova2="io sono un altra variabile"
+
+var variabile_write="io scrivo la variabile in out"
+
+var inner_variable="io scrivo la variabile in ID"
+
+var pop_up="io sono un popup e appaio in un alert"
+
+var x=7
+var y=2
+
+/* log command
+
+*/
+
+window.alert(pop_up)
+console.log(variabile_prova)
+console.log(variabile_prova2)
+
+document.write(variabile_write)
+
+//funzione
+
+function somma() {
+
+    return x+y;
+}
+    //log funzione
+
+console.log (somma())
+
+
+
