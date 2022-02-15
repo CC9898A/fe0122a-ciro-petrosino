9f89912f3363cdbe9b82d4da3b35195fae0060e2
CC9898A <cirpetrosino98@gmail.com>
2022-02-15 12:23:51 +0100

    modifica

---
+++ s4/g2/js/script.js
@@ -0,0 +1,50 @@
+//Operazioni somma e sottrazione
+
+//somma
+
+var a=25
+var b=22
+var c=a+b
+
+console.log(c)
+
+//sottrazione
+var d=20
+var e=5
+var f=d-e
+
+console.log(f)
+
+//boolean con +stringa
+
+var presenza=true
+
+var nome="matteo"
+
+console.log(presenza+nome)
+
+
+//var let const
+
+//valore var (allocazione di dato)
+
+var origine="italiana"
+
+console.log(origine)
+
+origine="tedesca"
+
+console.log(origine)
+
+{let origine="ebraica"
+console.log(origine)
+
+origine="spagnola"
+console.log(origine)
+}
+
+{const pianeta="terra"
+console.log(pianeta)
+}
+
+
