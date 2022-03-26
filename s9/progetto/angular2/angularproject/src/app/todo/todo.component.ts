e72a8bb0889a294b45107f88194d22a1ac012ecb
CC9898A <cirpetrosino98@gmail.com>
2022-03-26 23:13:01 +0100

    angular

---
+++ s9/progetto/angular2/angularproject/src/app/todo/todo.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',//nome componente da richiamare nell HTML dell'appcomponent
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TODOComponent implements OnInit {
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+senddata():any{
+
+
+
+
+
+
+
+}
+
+
+}
+
+
